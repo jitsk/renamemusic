b95556ef2d1b2eafc140300902bd183a31e9f801
jithinsebastian <jithinsebastian@qburst.com>
2017-01-24 17:00:08 +0530

    Added solution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # renamemusic
+
+Rename music, remove the leading , trailing nuisance words. Rename as Song name - Singer name and save them. Also ensures no duplicate files are there.
---
+++ rename.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+from guessit import guessit
+
+MUSICDB = '/home/user/Music'
+
+def find(folder):
+    for song in os.listdir(folder):
+        songInfo = guessit(song)
+        try:
+            renameFile(folder + '/' + song, folder + '/' + songInfo['episode_title'] + '-' + songInfo['title'] + '.mp3')
+        except:
+            try:
+                renameFile(folder + '/' + song, folder + '/' + songInfo['alternative_title'] + '-' + songInfo['title'] + '.mp3')
+            except:
+                renameFile(folder + '/' + song, folder + '/' + songInfo['title'] + '.mp3')
+
+def renameFile(oldfile, newFile):
+    shutil.move(oldfile, newFile)
+
+find(MUSICDB)
